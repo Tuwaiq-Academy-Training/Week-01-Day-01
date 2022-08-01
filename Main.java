4504f3455701983f01f820af7a410c262987209c
Saleh-SAFCSP <93971372+Saleh-SAFCSP@users.noreply.github.com>
2022-08-01 14:50:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,194 @@
+package com.company;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+     //   System.out.print("Hello World");
+
+
+
+        // All programming is operation and data
+
+        // Types of data
+        // 1- primitive
+        // 2- reference
+
+
+//         primitive datatype :
+//         integer (int) , long  , double , float , boolean , char , byte , short
+//
+        // integer , long
+//        System.out.println(10);
+//        System.out.println(10L);
+//        // Integer
+//        System.out.println(214748364);
+//        // Long
+//        System.out.println(9223372036854775807L);
+
+
+        //  double , float
+//        System.out.println(10.5D);
+//        System.out.println(10.8F);
+//        System.out.println(Double.MAX_VALUE);
+//        System.out.println(Float.MAX_VALUE);
+
+        // boolean
+//        System.out.println(true);
+//        System.out.println(false);
+
+        // char
+       // System.out.println('S'+'A'+'L'+'E'+'H');
+
+
+        // reference
+      //  System.out.println("SALEH D08QWYFOWQYG QWGY08Q");
+
+        // variables
+        // < Datatype > < Name > =  < Value >
+        // Rules of naming variable :
+        // 1- Doesn't start with number
+        // 2- Only contain numbers and char and $ and _
+        // 3- Not reserved word
+
+//        int price =  10;
+//        int total=price+10;
+//        System.out.println(total);
+
+
+//        int number1=5;
+//        int number2=10;
+//        int total=number1+number2;
+//
+//        double price= 15.18;
+//        boolean isActive=true;
+//       // System.out.println(total);
+//        String name="Saleh";
+//        System.out.println(name);
+//        name="Ali";
+//        System.out.println(name);
+
+
+        // Operation
+        // Types of operation in java
+        // 1- Arithmetic operation
+        // 2- Compression operation
+        // 3- Logical operation
+
+
+        // Arithmetic operation
+        // + - * / %
+
+        int number1=10;
+        int number2=2;
+
+//        int total= number1 % number2;
+//
+//        System.out.println(total);
+//
+//        String firstName="Saleh";
+//        String lastName="Alibrahim";
+//
+//        String fullName=firstName+" "+lastName;
+//        System.out.println(fullName);
+
+       // System.out.println('A'+'B');
+
+        // Compression operation
+        // < > == != <= >=
+//        System.out.println(10<2);
+//        System.out.println(20<30);
+
+//        System.out.println(100==100);
+//        System.out.println("Ali"=="Ali");
+        // System.out.println(15>=10);
+
+        // Logical operation
+        // && || !
+       // System.out.println(10>2 && 5<3&& 20 > 10);
+         // System.out.println(10>2 || 5<3 || 10 < 3);
+//          System.out.println(!(20<30));
+        //System.out.println(!(10>2 && 5<3&& 20 > 10));
+
+
+        // Pillars of programming
+        // 1- Sequence
+        // 2- Selection
+        // 3- Iteration
+
+
+        // Compression
+        // if else switch
+        String user="Saleh";
+        String role="Admin";
+
+//        if(role=="Admin") {
+//            System.out.println("Hey Admin");
+//        } else {
+//            System.out.println("Hey User");
+//        }
+
+
+        // Iteration
+        // for while do-while
+//        System.out.println("Hey Saleh ");
+//        System.out.println("Hey Saleh ");
+//        System.out.println("Hey Saleh ");
+//        System.out.println("Hey Saleh ");
+//        System.out.println("Hey Saleh ");
+//        System.out.println("Hey Saleh ");
+        // for (   ;     ;   )
+
+//        for( int i=1  ;i <= 5; i=i+1 ){
+//            System.out.println("Hey Saleh");
+//        }
+
+//        for( int i=1  ;i <= 5; i=i+1 ){
+//            System.out.println("Hey Saleh");
+//        }
+//
+        int i=1;
+        while(  i > 5 ){
+            System.out.println("Hey Saleh");
+            i=i+1;
+        }
+
+//        int i=1;
+//        do {
+//            System.out.println("Hey Saleh");
+//            i=i+1;
+//        }while ( i > 5 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //
+    }
+}
